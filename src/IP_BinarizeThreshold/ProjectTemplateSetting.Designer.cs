817b5af0574ef2230192f6769fe853a79cd7154f
masjinno <masjinno@gmail.com>
2016-09-21 00:59:14 +0900

    #６ 二値化閾値法のプロジェクトを追加

---
+++ src/IP_BinarizeThreshold/ProjectTemplateSetting.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace IP_BinarizeThreshold.Setting    //RENAME
+{
+    partial class ProjectTemplateSetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cancel_Button = new System.Windows.Forms.Button();
+            this.OK_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Cancel_Button
+            // 
+            this.Cancel_Button.Location = new System.Drawing.Point(116, 227);
+            this.Cancel_Button.Name = "Cancel_Button";
+            this.Cancel_Button.Size = new System.Drawing.Size(75, 23);
+            this.Cancel_Button.TabIndex = 0;
+            this.Cancel_Button.Text = "キャンセル";
+            this.Cancel_Button.UseVisualStyleBackColor = true;
+            this.Cancel_Button.Click += new System.EventHandler(this.Cancel_Button_Click);
+            // 
+            // OK_Button
+            // 
+            this.OK_Button.Location = new System.Drawing.Point(197, 227);
+            this.OK_Button.Name = "OK_Button";
+            this.OK_Button.Size = new System.Drawing.Size(75, 23);
+            this.OK_Button.TabIndex = 1;
+            this.OK_Button.Text = "OK";
+            this.OK_Button.UseVisualStyleBackColor = true;
+            this.OK_Button.Click += new System.EventHandler(this.OK_Button_Click);
+            // 
+            // ProjectTemplateSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.OK_Button);
+            this.Controls.Add(this.Cancel_Button);
+            this.Name = "ProjectTemplateSetting";   //RENAME
+            this.Text = "ProjectTemplateSetting";   //RENAME
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cancel_Button;
+        private System.Windows.Forms.Button OK_Button;
+    }
+}
